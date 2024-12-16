b7db8c2ec0ea0215f829b731555342441a55177b
Amin394 <amin394@outlook.com>
2024-12-17 00:43:10 +0100

    allowed only comment writer to edit or delete

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,16 +1,16 @@
 import Message from "../models/messageModel.js";
+import User from "../models/userModel.js";
 
 export async function createMessage(req, res) {
   try {
     const { username, password, content, parent } = req.body;
-    const user = await User.findOne({ where: { username } });
-    if (!user) {
-      return res.status(401).json({ message: "Invalid username or password" });
+
+    const user = await User.findByPk(username);
+    if (!user && user.password !== password) {
+      return res.status(401).json({ message: "Invalid credentials!" });
     }
-    if (user.password !== password) {
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-    const message = await Message.create({ username, content, parent });
+
+    const message = await Message.create({ user, content, parent });
     res.status(201).json(message);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -21,8 +21,9 @@ export async function readMessage(req, res) {
   try {
     const message = await Message.findByPk(req.params.id);
     if (!message) {
-      return res.status(404).json({ message: "Comment not found" });
+      return res.status(404).json({ message: "Comment not found!" });
     }
+
     res.status(200).json(message);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -31,11 +32,20 @@ export async function readMessage(req, res) {
 
 export async function updateMessage(req, res) {
   try {
-    const { content } = req.body;
+    const { username, password, content } = req.body;
+
+    const user = await User.findByPk(username);
+    if (!user && user.password !== password) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
     const message = await Message.findByPk(req.params.id);
     if (!message) {
-      return res.status(404).json({ message: "Comment not found" });
+      return res.status(404).json({ message: "Comment not found!" });
     }
+    if (message.username != username) {
+      return res.status(403).json({ message: "Access forbidden!" });
+    }
+
     message.content = content || message.content;
     message.date = new Date();
     await message.save();
@@ -47,12 +57,22 @@ export async function updateMessage(req, res) {
 
 export async function deleteMessage(req, res) {
   try {
+    const { username, password } = req.body;
+
+    const user = await User.findByPk(username);
+    if (!user && user.password !== password) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
     const message = await Message.findByPk(req.params.id);
     if (!message) {
-      return res.status(404).json({ message: "Comment not found" });
+      return res.status(404).json({ message: "Comment not found!" });
     }
+    if (message.username != username) {
+      return res.status(403).json({ message: "Access forbidden!" });
+    }
+
     await message.destroy();
-    res.status(200).json({ message: "Comment deleted" });
+    res.status(200).json({ message: "Comment deleted." });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ import User from "../models/userModel.js";
 export async function addUser(req, res) {
   try {
     const { username, password } = req.body;
+
     const user = await User.create({ username, password });
     res.status(201).json(user);
   } catch (error) {
@@ -16,6 +17,7 @@ export async function getUser(req, res) {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
+    
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
